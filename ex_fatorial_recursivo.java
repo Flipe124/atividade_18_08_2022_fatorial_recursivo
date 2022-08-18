d1143097fd237532f887fb5dc69f28d26a283360
felipe <100162163+devwdevel5@users.noreply.github.com>
2022-08-18 20:07:30 -0300

    att

---
+++ ex_fatorial_recursivo.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class ex_fatorial_recursivo {
+    public static void main(String[] args){
+        int fatorial;
+        Scanner scan = new Scanner(System.in);
+
+
+        System.out.println("Digite um número para calcular seu fatorial:");
+        fatorial = scan.nextInt();
+
+        System.out.println(calFatorial(fatorial));
+
+    }
+    private  static int calFatorial(int i){
+
+        if (i == 0){
+            return 1;
+        } else {
+            return i * calFatorial(i - 1);
+        }
+    }
+}
